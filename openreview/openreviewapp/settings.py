df056b71b9ceacddbb52c6ee62a7e1eea6d5fda6
Jure Cuhalev <jure@cuhalev.com>
2021-06-15 10:42:30 +0200

    Fixes imports so that ./manage.py migrate works now

--- openreview/openreviewapp/settings.py
+++ openreview/openreviewapp/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'openreview.urls'
+ROOT_URLCONF = 'openreviewapp.urls'
 
 TEMPLATES = [
     {
