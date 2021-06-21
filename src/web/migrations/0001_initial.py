d6103b55766364bf3ef30e80de773c265bc584e0
Jure Cuhalev <jure@cuhalev.com>
2021-06-21 18:47:48 +0200

    Initial Entry model

--- src/web/migrations/0001_initial.py
+++ src/web/migrations/0001_initial.py
@@ -15,21 +15,43 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Project',
+            name="Project",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=120)),
-                ('is_active', models.BooleanField(default=True)),
-                ('created', models.DateTimeField(auto_now_add=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=120)),
+                ("is_active", models.BooleanField(default=True)),
+                ("created", models.DateTimeField(auto_now_add=True)),
             ],
         ),
         migrations.CreateModel(
-            name='UserProfile',
+            name="UserProfile",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('is_active', models.BooleanField(default=True)),
-                ('projects', models.ManyToManyField(to='web.Project')),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("is_active", models.BooleanField(default=True)),
+                ("projects", models.ManyToManyField(to="web.Project")),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
--- src/web/models.py
+++ src/web/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import JSONField
 
 
 class Project(models.Model):
@@ -16,3 +17,22 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     projects = models.ManyToManyField(Project)
     is_active = models.BooleanField(default=True)
+
+
+class Entry(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    title = models.TextField()
+    key = models.CharField(null=True, blank=True, max_length=120)
+
+    data = JSONField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Entry"
+        verbose_name_plural = "Entries"
+
+    def __str__(self):
+        if self.key:
+            return "#{} - {}".format(self.key, self.title)
+        else:
+            return self.title
